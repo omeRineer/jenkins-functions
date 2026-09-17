bb32c216be77c3376c1cfa165a48284e71756eaa
Ömer Faruk Sandıkçı <sandikci_omer@hotmail.com>
2026-09-17 13:54:44 +0300

    .

--- sendNotification.groovy
+++ sendNotification.groovy
@@ -1,13 +1,19 @@
 def call(Map config = [:]) {
-    withCredentials([string(credentialsId: 'INTEGRATION_API', variable: 'API_URL'),
-                     string(credentialsId: 'INTEGRATION_API_KEY', variable: 'API_KEY')]) {
+    // Değerler boş gelirse varsayılan olarak Jenkins ortam değişkenlerini kullanır
+    def jobName = config.jobName ?: env.JOB_NAME
+    def status = config.status ?: currentBuild.currentResult
+
+    withCredentials([
+        string(credentialsId: 'INTEGRATION_API', variable: 'API_URL'),
+        string(credentialsId: 'INTEGRATION_API_KEY', variable: 'API_KEY')
+    ]) {
         sh """
-            curl --location '${API_URL}/Notifications/create' \
-                --header 'Content-Type: application/json' \
-                --header 'Fluxify-Api-Key: ${API_KEY}' \
+            curl --location "\${API_URL}/Notifications/create" \
+                --header "Content-Type: application/json" \
+                --header "Fluxify-Api-Key: \${API_KEY}" \
                 --data '{
                     "Title": "Deploy Tamamlandı",
-                    "Summary": "${config.jobName} :: ${config.status}",
+                    "Summary": "${jobName} :: ${status}",
                     "Type": "info",
                     "To": {
                         "Users": null,
