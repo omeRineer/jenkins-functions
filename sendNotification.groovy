daff60b83120c4d4a5b598e1c604a7bf4dcbe36a
Ömer Faruk Sandıkçı <sandikci_omer@hotmail.com>
2026-09-17 13:46:53 +0300

    .

--- sendNotification.groovy
+++ sendNotification.groovy
@@ -1,7 +1,4 @@
 def call(Map config = [:]) {
-    def credId = config.credentialsId ?: 'MY_API_TOKEN'
-    def endpoint = config.apiUrl ?: 'https://api.domain.com/v1/notifications'
-
     withCredentials([string(credentialsId: 'INTEGRATION_API', variable: 'API_URL'),
                      string(credentialsId: 'INTEGRATION_API_KEY', variable: 'API_KEY')]) {
         sh """
